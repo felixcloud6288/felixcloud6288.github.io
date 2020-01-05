e0aedbcaccffa52bf4cf5ed6162101d5423bcfd3
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-01-05 11:35:15 -0500

    Beginning work on frontend of password generator

--- assets/passwordGen/password.js
+++ assets/passwordGen/password.js
@@ -8,105 +8,122 @@
 */
 
 // Get the User's input on what the password needs
-var password = {
-    length: prompt("How many characters will the password have?"),
-    capital: confirm("Are there capital letters?"),
-    lowercase: confirm("Are there lowercase letters?"),
-    numbers: confirm("Are there numbers?"),
-    special: confirm("Are there special characters?"),
+var length,capital,lowercase,numbers,special
 
-    makePassword: function(string){
-        // Generate a random number from 0-3 to determine what will go in each index of the password
-        // 0 = capital
-        // 1 = lowercase
-        // 2 = number
-        // 3 = special character
-        for(var i=0; i<this.length;i++)
-        {
-            var x = Math.floor(Math.random() * 4)
+// Prompts the user for parameters
+function getParameters(){
+    
+    length = prompt("How many characters will the password have?")
+    length = parseInt(length, 10)
+    // Security net to ensure user does not set length to a string or negative number
+    while (isNaN(length) || length < 3){
+        length = prompt("How many characters will the password have?")
+        length = parseInt(length, 10)
+    }
+    capital = confirm("Are there capital letters?")
+    lowercase = confirm("Are there lowercase letters?")
+    numbers = confirm("Are there numbers?")
+    special = confirm("Are there special characters?")
+}
+function makePassword(string){
+    getParameters()
 
-            if (x == 0 && this.capital == true)
-                string[i] = this.generateCapital();
-            else if (x == 1 && this.lowercase == true)
-                string[i] = this.generateLowerCase();
-            else if (x == 2 && this.numbers == true)
-               string[i] = this.generateNumber();
-            else if (x == 3 && this.special == true)
-                string[i] = this.generateSpecial();
-            else{
-                i--;
-                continue;
-            }
-
-            string[i] = String.fromCharCode(string[i]);
-            console.log("Current string = "+ string.join(""))
+    // Generate a random number from 0-3 to determine what will go in each index of the password
+    // 0 = capital
+    // 1 = lowercase
+    // 2 = number
+    // 3 = special character
+    for(var i=0; i<length;i++)
+    {
+        var x = Math.floor(Math.random() * 4)
+        if (x == 0 && capital == true)
+            string[i] = generateCapital();
+        else if (x == 1 && lowercase == true)
+            string[i] = generateLowerCase();
+        else if (x == 2 && numbers == true)
+           string[i] = generateNumber();
+        else if (x == 3 && special == true)
+            string[i] = generateSpecial();
+        else{
+            i--;
+            continue;
         }
-        string = this.makeEmergencyValues(string)
-        return string;
-    },
+        // convert ints to ASCII values
+        string[i] = String.fromCharCode(string[i]);
+        console.log("Current string = "+ string.join(""))
+    }
+    // An additional precaution to ensure at least one of every char type is put in the password
+    string = makeEmergencyValues(string)
+    return string;
+}
 
-    generateCapital(){
-        return Math.floor(Math.random()*26 + 65)
-    },
-    generateLowerCase(){
-        return Math.floor(Math.random()*26 + 97)
-    },
-    generateNumber(){
-        return Math.floor(Math.random()*10 + 48)
-    },
-    generateSpecial(){
-        var i = Math.floor(Math.random() * 4)
-        if (i == 0)
-        {
-            return Math.floor(Math.random()*15 + 33)
-        }
-        else if(i == 1){
-            return Math.floor(Math.random()*7 + 58)
-        }
-        else if(i == 2){
-            return Math.floor(Math.random()*6 + 91)
-        }
-        else if(i == 3){
-            return Math.floor(Math.random()*4 + 123)
-        }
-    },
-    // Create a capital, lowercase, number, and special char that will definitely be in the password
-    makeEmergencyValues: function(string){
-        console.log(string.join(''))
-        // Determine where they will go
-        var i = Math.floor(Math.random()* this.length-1) 
-        var j = Math.floor(Math.random()* this.length-1)
-        var k = Math.floor(Math.random()* this.length-1) 
-        var l = Math.floor(Math.random()* this.length-1) 
-
-        // Ensure none of them go to the same place
-        while (j == i || j == k || j == l){
-            j = Math.floor(Math.random()* this.length-1)
-        }
-        while (k == i || k == j || k == l){
-            k = Math.floor(Math.random()* this.length-1)
-        }
-        while (l == i || l == j || l == k){
-            l = Math.floor(Math.random()* this.length-1)
-        }
-
-        if (this.capital == true)
-            string[i] = String.fromCharCode(this.generateCapital());
-        if (this.lowercase == true)
-          string[j] = String.fromCharCode(this.generateLowerCase());
-        if (this.numbers == true)
-           string[k] = String.fromCharCode(this.generateNumber());
-        if (this.special == true)
-            string[l] = String.fromCharCode(this.generateSpecial());
-
-
-        console.log("New password is" + string.join(""))
-
-
-       // string[i] = String.fromCharCode(Math.floor(Math.random() *26 + 65))
-        return string;
+function generateCapital(){
+    return Math.floor(Math.random()*26 + 65)
+}
+function generateLowerCase(){
+    return Math.floor(Math.random()*26 + 97)
+}
+function generateNumber(){
+    return Math.floor(Math.random()*10 + 48)
+}
+// Because special characters are broken into 4 sequential groups in ASCII, we first generate a random number from 0-3 which will decide which set of ASCII values will be pulled from
+function generateSpecial(){
+    var i = Math.floor(Math.random() * 4)
+    if (i == 0)
+    {
+        return Math.floor(Math.random()*15 + 33)
+    }
+    else if(i == 1){
+        return Math.floor(Math.random()*7 + 58)
+    }
+    else if(i == 2){
+        return Math.floor(Math.random()*6 + 91)
+    }
+    else if(i == 3){
+        return Math.floor(Math.random()*4 + 123)
     }
 }
+// Create a capital, lowercase, number, and special char that will definitely be in the password
+function makeEmergencyValues(string){
+    console.log(string.join(''))
+    // Determine where each item will go
+    var i = Math.floor(Math.random()* this.length) 
+    var j = Math.floor(Math.random()* this.length)
+    var k = Math.floor(Math.random()* this.length) 
+    var l = Math.floor(Math.random()* this.length) 
+
+    // Ensure none of the values are the same
+    while (j == i || j == k || j == l){
+        j = Math.floor(Math.random()* length)
+    }
+    while (k == i || k == j || k == l){
+        k = Math.floor(Math.random()* length)
+    }
+    while (l == i || l == j || l == k){
+        l = Math.floor(Math.random()* length)
+    }
+
+    if (capital == true)
+        string[i] = String.fromCharCode(generateCapital());
+    if (lowercase == true)
+        string[j] = String.fromCharCode(generateLowerCase());
+    if (numbers == true)
+       string[k] = String.fromCharCode(generateNumber());
+    if (special == true)
+        string[l] = String.fromCharCode(generateSpecial());
+
+
+    console.log("New password is" + string.join(""))
+
+
+    // string[i] = String.fromCharCode(Math.floor(Math.random() *26 + 65))
+    return string;
+}
+
+// adding an eventListener to the HTML button, when clicked, everything is run
+document.body.children[1].addEventListener("click", function(){
+
 var string = []
-string = password.makePassword(string)
+string = makePassword(string)
 console.log(string.join(''))
+})
