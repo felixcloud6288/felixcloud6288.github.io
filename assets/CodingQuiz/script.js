a797206c2b03cf624cd81592680cafea9fbefd07
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-01-10 10:08:45 -0500

    Beginning to outline the process

--- assets/CodingQuiz/script.js
+++ assets/CodingQuiz/script.js
@@ -1,3 +1,4 @@
+// Quiz Content
 var quiz = {
     questions: [
         "Which of the following languages are not used for web development?", 
@@ -21,3 +22,23 @@ var quiz = {
         [false, false, true, false]
     ]
 }
+function beginQuiz(event){
+    console.log("Entered quiz")
+    // Create the components of the page
+    var questionCount = document.createElement("div")
+    var question = document.createElement("div")
+    var button1 = document.createElement("button")
+    var button2 = document.createElement("button")
+    var button3 = document.createElement("button")
+    var button4 = document.createElement("button")
+    // Add attributes to them. Buttons are given an ID to determine which has the right answer
+    
+    // Append to the page
+
+    // loop through the quiz
+}
+
+var button = document.createElement("button")
+button.textContent = "Click here to begin"
+button.addEventListener("click", beginQuiz)
+document.body.appendChild(button)
