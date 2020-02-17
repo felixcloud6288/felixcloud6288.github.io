52e53303c74c16c6257aa614504855ee938cef1e
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-02-17 18:52:26 -0500

    Adding a Resume Builder app

---
+++ assets/ResumeBuilder/Develop/index.js
@@ -0,0 +1,16 @@
+const inquirer = require("inquirer");
+const generateHTML = require("./generateHTML.js");
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+    generateHTML.test();
+}
+
+init();
