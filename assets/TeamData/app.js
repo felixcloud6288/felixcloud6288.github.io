f0709eba8282466a9605cf9f60be72da022e6ab5
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-02-24 11:03:26 -0500

    Working on a basic menu in app.js. Right now it loops

--- assets/TeamData/app.js
+++ assets/TeamData/app.js
@@ -1,2 +1,23 @@
 const inquirer = require("inquirer");
-//const Employee = require("./lib/Employee");
+const manager = require("./lib/Manager");
+const intern = require("./lib/Intern");
+const engineer = require("./lib/Engineer");
+
+const team = [];
+
+function menu(){
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What do you want to do?",
+        choices: ["Add Manager", "Add Engineer", "Add Intern", "Create Profile", "Exit"]
+    }).then(function(response){
+        console.log(response);
+        if(response.action === "Exit"){
+            return;
+        }
+        menu();
+    })
+
+}
+menu();
