b0da07f00fd918524cfdddc2d816ebf1238a1ba0
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-03 09:29:27 -0500

    Made basic server.js file

--- assets/noteMaker/server.js
+++ assets/noteMaker/server.js
@@ -1,1 +1,17 @@
-var express = require("express");
+const express = require("express");
+
+const app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
