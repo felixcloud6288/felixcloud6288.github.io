4444a44302af67358138b51b11c0060c6e238165
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-04-12 12:55:15 -0400

    Made routing capabilities. Program jumps to provided pages

--- exercise/routes/api.js
+++ exercise/routes/api.js
@@ -1,35 +1,8 @@
 const router = require("express").Router();
-const Transaction = require("../models/exercises.js");
+const Exercises = require("../models/exercises.js");
 
-router.post("/api/transaction", ({ body }, res) => {
-    Transaction.create(body)
-        .then(dbTransaction => {
-            res.json(dbTransaction);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
-
-router.post("/api/transaction/bulk", ({ body }, res) => {
-    Transaction.insertMany(body)
-        .then(dbTransaction => {
-            res.json(dbTransaction);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
-
-router.get("/api/transaction", (req, res) => {
-    Transaction.find({})
-        .sort({ date: -1 })
-        .then(dbTransaction => {
-            res.json(dbTransaction);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
+router.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+})
 
 module.exports = router;
