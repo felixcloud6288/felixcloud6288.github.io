c610563e4d5fffcd953185240bc71feb7da0920d
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-05-01 16:04:26 -0400

    Added navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/nav"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav fixed="top"/>
+        <Switch>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
