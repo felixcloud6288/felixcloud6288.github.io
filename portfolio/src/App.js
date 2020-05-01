473395d1ba07acebe6b48a5acbd1b80fb0ce8148
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-05-01 18:56:39 -0400

    Home page made

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import "./css/style.css"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/nav"
+import Home from "./pages/home"
 function App() {
   return (
     <Router>
       <div>
         <Nav fixed="top"/>
         <Switch>
-
+          <Route exact path="/" component={Home}/>
         </Switch>
       </div>
     </Router>
